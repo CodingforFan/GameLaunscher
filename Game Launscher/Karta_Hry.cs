d134b06172c4b1f3c8a35ddc3bf9d13c43673aff
GamerClassN7 <gamerclassn7@gmail.com>
2017-02-02 09:44:40 +0100

    Bether search in game database

--- Game Launscher/Karta_Hry.cs
+++ Game Launscher/Karta_Hry.cs
@@ -46,8 +46,8 @@ namespace Game_Launscher
 //			
 //			pictureBox1.Load("https://images.igdb.com/igdb/image/upload/t_cover_big/jusjbgw2hb80sgyltvlw.jpg");
 			using(System.Net.WebClient client = new System.Net.WebClient()){
-				string htmlcode = client.DownloadString("https://www.igdb.com/games/" + G_Name.Replace(" ","-"));
-				MessageBox.Show("https://www.igdb.com/games/" + G_Name.Replace(" ","-"));
+				string htmlcode = client.DownloadString("https://www.igdb.com/search?utf8=✓&type=1&q=" + G_Name.Replace(".exe",string.Empty).Replace(" ","%"));
+				MessageBox.Show("https://www.igdb.com/search?utf8=✓&type=1&q=" + G_Name.Replace(".exe",string.Empty).Replace(" ","%"));
 				textBox1.Text = htmlcode;
 			}
 	
