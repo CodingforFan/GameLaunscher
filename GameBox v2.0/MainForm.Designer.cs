e4de0484a40f3681d843ae498998e2028a755aab
Silentis <SilentisLP@gmail.com>
2017-03-13 20:57:06 +0100

    Ještě něco

--- GameBox v2.0/MainForm.Designer.cs
+++ GameBox v2.0/MainForm.Designer.cs
@@ -6,7 +6,7 @@
  * 
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
-namespace GameBox_v2._
+namespace GameBox_v2
 {
 	partial class MainForm
 	{
@@ -17,6 +17,12 @@ namespace GameBox_v2._
 		private System.Windows.Forms.Button button1;
 		private System.Windows.Forms.OpenFileDialog openFileDialog1;
 		private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+		private System.Windows.Forms.Timer update;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem změnitObrázekToolStripMenuItem;
+		private System.Windows.Forms.OpenFileDialog openFileDialog2;
+		private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem1;
 		
 		/// <summary>
 		/// Disposes resources used by the form.
@@ -39,9 +45,17 @@ namespace GameBox_v2._
 		/// </summary>
 		private void InitializeComponent()
 		{
+			this.components = new System.ComponentModel.Container();
 			this.button1 = new System.Windows.Forms.Button();
 			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
 			this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+			this.update = new System.Windows.Forms.Timer(this.components);
+			this.label2 = new System.Windows.Forms.Label();
+			this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+			this.změnitObrázekToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+			this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+			this.contextMenuStrip1.SuspendLayout();
 			this.SuspendLayout();
 			// 
 			// button1
@@ -61,21 +75,71 @@ namespace GameBox_v2._
 			// 
 			// flowLayoutPanel1
 			// 
-			this.flowLayoutPanel1.Location = new System.Drawing.Point(29, 59);
+			this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 47);
 			this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-			this.flowLayoutPanel1.Size = new System.Drawing.Size(558, 173);
+			this.flowLayoutPanel1.Size = new System.Drawing.Size(786, 240);
 			this.flowLayoutPanel1.TabIndex = 1;
+			// 
+			// update
+			// 
+			this.update.Enabled = true;
+			this.update.Interval = 300;
+			this.update.Tick += new System.EventHandler(this.UpdateTick);
+			// 
+			// label2
+			// 
+			this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+			| System.Windows.Forms.AnchorStyles.Right)));
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(399, 16);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(35, 13);
+			this.label2.TabIndex = 2;
+			this.label2.Text = "label2";
+			this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
+			// contextMenuStrip1
+			// 
+			this.contextMenuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+			this.změnitObrázekToolStripMenuItem,
+			this.toolStripMenuItem1});
+			this.contextMenuStrip1.Name = "contextMenuStrip1";
+			this.contextMenuStrip1.Size = new System.Drawing.Size(181, 48);
+			// 
+			// změnitObrázekToolStripMenuItem
+			// 
+			this.změnitObrázekToolStripMenuItem.Name = "změnitObrázekToolStripMenuItem";
+			this.změnitObrázekToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+			this.změnitObrázekToolStripMenuItem.Text = "Změnit obrázek";
+			this.změnitObrázekToolStripMenuItem.Click += new System.EventHandler(this.ZměnitObrázekToolStripMenuItemClick);
+			// 
+			// toolStripMenuItem1
+			// 
+			this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+			this.toolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+			this.toolStripMenuItem1.Text = "toolStripMenuItem1";
+			this.toolStripMenuItem1.Click += new System.EventHandler(this.ToolStripMenuItem1Click);
+			// 
+			// openFileDialog2
+			// 
+			this.openFileDialog2.FileName = "openFileDialog2";
+			this.openFileDialog2.FileOk += new System.ComponentModel.CancelEventHandler(this.OpenFileDialog2FileOk);
 			// 
 			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(810, 299);
+			this.Controls.Add(this.label2);
 			this.Controls.Add(this.flowLayoutPanel1);
 			this.Controls.Add(this.button1);
 			this.Name = "MainForm";
 			this.Text = "GameBox v2.0";
+			this.Activated += new System.EventHandler(this.MainFormActivated);
+			this.Load += new System.EventHandler(this.MainFormLoad);
+			this.contextMenuStrip1.ResumeLayout(false);
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 	}
--- GameBox v2.0/MainForm.cs
+++ GameBox v2.0/MainForm.cs
@@ -10,14 +10,28 @@ using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
+using System.IO;
 
-namespace GameBox_v2._
+
+namespace GameBox_v2
 {
 	/// <summary>
 	/// Description of MainForm.
 	/// </summary>
 	public partial class MainForm : Form
-	{
+	{		
+		static string current_item_name = null;
+		static Control current_item = null;
+		public List <string> datas = new List<string>();
+		public System.IO.StreamReader sr;
+		public System.IO.StreamWriter sw;
+		public List <string> saveTime;
+		public bool gameIsRun = false;
+		public int activeGame = -1;
+		public string processName;
+		public System.Diagnostics.Process process;
+		public string listing = "";
+		
 		public MainForm()
 		{
 			//
@@ -28,6 +42,44 @@ namespace GameBox_v2._
 			//
 			// TODO: Add constructor code after the InitializeComponent() call.
 			//
+		}
+		void MainFormLoad(object sender, EventArgs e)
+		{
+			datas = new List<string>();
+			if(File.Exists("./config/datalib.glconfig")){
+				int countLines = File.ReadAllLines("./config/datalib.glconfig").Length;
+				sr = new StreamReader("./config/datalib.glconfig");
+				if(sr.ReadToEnd().Length > 0){
+					sr.Close();
+					sr = new StreamReader("./config/datalib.glconfig");
+					for(int i = 0; i < countLines; i++){
+						datas.Add(sr.ReadLine());
+					}
+					for(int g = 0; g < datas.Count; g++){
+						string a = "";
+						int num = 0;
+						if(datas[g] != null){
+							foreach(string d in datas[g].Split('|')){
+								if(num == 2){
+									a = d;
+								}
+								num ++;
+							}
+						}
+						if(a != ""){
+							
+							additem(a);
+						}
+					}
+				}
+				sr.Close();
+			}else{
+				if(!Directory.Exists("./config")){
+					Directory.CreateDirectory("./config");
+				}
+				File.CreateText("./config/datalib.glconfig").Close();
+				datas = new List<string>();
+			}
 		}
 		void Button1Click(object sender, EventArgs e)
 		{
@@ -68,6 +120,155 @@ namespace GameBox_v2._
 			mustek:;
 			
 		}
+		Control GetControlUnderMouse() {
+    		foreach ( Control c in flowLayoutPanel1.Controls ) {
+        		if ( c.Bounds.Contains(flowLayoutPanel1.PointToClient(MousePosition)) ) {
+					return c;
+					
+         		}
+    		}
+			return null;
+		}
+		void Item_Click(object sender, MouseEventArgs e)
+        {
+			current_item = GetControlUnderMouse();
+			current_item_name = Path.GetFileName(current_item.Name);
+			
+			if (e.Button == MouseButtons.Left){
+				ButtonSet(System.Diagnostics.Process.Start(current_item.Name), int.Parse(current_item.Tag.ToString()));
+			}else if (e.Button == MouseButtons.Right){
+				contextMenuStrip1.Show( MousePosition); 
+				//MessageBox.Show(GetControlUnderMouse().Name);
+				//new Karta_Hry(current_item_name).Show();
+			}
+		}
+		void UpdateTick(object sender, EventArgs e)
+		{
+			//CreateGraphics().DrawRectangle(new Pen(Color.Red, 1), new Rectangle(0, 0+30, Width-1, Height-31));
+			label2.Text = DateTime.Now.ToString("HH:mm");
+		}
 		
+		public void ButtonSet(System.Diagnostics.Process processe, int ID){
+			gameIsRun = true;
+			process = processe;
+			processName = process.ProcessName;
+			activeGame = ID;
+			saveTime = new List<string>();
+			var sets = DateTime.Now.ToString("HH;mm;ss");
+			saveTime.Add(sets.Split(';')[0]);
+			saveTime.Add(sets.Split(';')[1]);
+			saveTime.Add(sets.Split(';')[2]);
+		}
+		void OpenFileDialog2FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+		{
+			string localfilenames = openFileDialog2.FileName;
+			if(File.Exists(localfilenames)){
+				Directory.CreateDirectory("./coverlib");
+				MessageBox.Show(current_item_name + current_item.ToString());
+				File.Copy(openFileDialog2.FileName, "./coverlib/" + current_item_name.Replace(".exe", string.Empty) + "." + localfilenames.Split('.')[1], true);
+
+				current_item.BackgroundImage = Image.FromFile("./coverlib/"+ current_item_name.Replace(".exe", string.Empty) +"." + localfilenames.Split('.')[1]);
+    		}	
+		}
+		void ZměnitObrázekToolStripMenuItemClick(object sender, EventArgs e)
+		{
+			openFileDialog2.ShowDialog();
+		}
+		void MainFormActivated(object sender, EventArgs e)
+		{
+			if(gameIsRun){
+				if(process.HasExited){
+					gameIsRun = false;
+					if(datas.Count > activeGame){
+						if(datas[activeGame] != null){
+							int num = 0;
+							foreach(string t in datas[activeGame].Split('|')){
+								if(num == 2){
+									datas[activeGame].Replace(t, HowLongPlay(t));
+								}
+								num++;
+							}
+						}else{
+							datas[activeGame] = processName.Replace(".exe", string.Empty) + "|" + HowLongPlay("00;00;00");
+						}
+					}else{
+						for(int i = 0; i <= activeGame; i++){
+							if(i == activeGame){
+								datas.Add(processName.Replace(".exe", string.Empty) + "|" + HowLongPlay("00;00;00"));
+							}else{
+								datas.Add(null);
+							}
+						}
+					}
+					activeGame = -1;
+					SaveData();
+				}
+			}
+		}
+		public void SaveData(){
+			if(System.IO.File.Exists("./config/datalib.glconfig")){
+				sw = new System.IO.StreamWriter("./config/datalib.glconfig", false);
+				for(int i = 0; i < datas.Count; i++){
+					if(datas[i] != null){
+						sw.Write(datas[i] + Environment.NewLine);
+					}
+				}
+				sw.Close();
+			}else{
+				sw = System.IO.File.CreateText("./config/datalib.glconfig");
+				for(int i = 0; i < datas.Count; i++){
+					if(datas[i] != null){
+						sw.Write(datas[i] + Environment.NewLine);
+					}
+				}
+				sw.Close();
+			}
+		}
+		
+		public string HowLongPlay(string lastPlayeTime){
+			List<string> nowTime = new List<string>();
+			var sets = DateTime.Now.ToString("HH;mm;ss");
+			nowTime.Add(sets.Split(';')[0]);
+			nowTime.Add(sets.Split(';')[1]);
+			nowTime.Add(sets.Split(';')[2]);
+			int num = 0;
+			string h = "00";
+			string m = "00";
+			string s = "00";
+			foreach(string one in lastPlayeTime.Split(';')){
+				int nt = int.Parse(nowTime[num]);
+				int st = int.Parse(saveTime[num]);
+				int o = int.Parse(one);
+				int hp = int.Parse(h);
+				int mp = int.Parse(m);
+				if(num == 0){
+					if(nt >= st){
+						h = (o + (nt - st)).ToString();
+					}else{
+						h = (o + (24 - st + nt)).ToString();
+					}
+				}else if(num == 1){
+					if(nt >= st){
+						m = (o + (nt - st)).ToString();
+					}else{
+						m = (o + (60 - st + nt)).ToString();
+						h = (hp - 1).ToString();
+					}
+				}else if(num == 2){
+					if(nt >= st){
+						s = (o + (nt - st)).ToString();
+					}else{
+						s = (o + (60 - st + nt)).ToString();
+						m = (mp - 1).ToString();
+					}
+				}
+				num++;
+			}
+			return (h + ";" + m + ";" + s);
+		}
+		void ToolStripMenuItem1Click(object sender, EventArgs e)
+		{
+			new Karta_Hry(current_item_name).Show();
+		}
 	}
 }
