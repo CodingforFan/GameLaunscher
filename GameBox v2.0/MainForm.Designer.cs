e9276e28ccd54267b7b315e3387e9db87743b91e
GamerClassN7 <gamerclassn7@gmail.com>
2017-03-13 19:54:12 +0100

    Secret

--- GameBox v2.0/MainForm.Designer.cs
+++ GameBox v2.0/MainForm.Designer.cs
@@ -15,6 +15,8 @@ namespace GameBox_v2._
 		/// </summary>
 		private System.ComponentModel.IContainer components = null;
 		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.OpenFileDialog openFileDialog1;
+		private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
 		
 		/// <summary>
 		/// Disposes resources used by the form.
@@ -38,22 +40,38 @@ namespace GameBox_v2._
 		private void InitializeComponent()
 		{
 			this.button1 = new System.Windows.Forms.Button();
+			this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+			this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
 			this.SuspendLayout();
 			// 
 			// button1
 			// 
-			this.button1.Location = new System.Drawing.Point(14, 28);
+			this.button1.Location = new System.Drawing.Point(12, 12);
 			this.button1.Name = "button1";
-			this.button1.Size = new System.Drawing.Size(172, 41);
+			this.button1.Size = new System.Drawing.Size(61, 20);
 			this.button1.TabIndex = 0;
-			this.button1.Text = "button1";
+			this.button1.Text = "Přidat";
 			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.Button1Click);
+			// 
+			// openFileDialog1
+			// 
+			this.openFileDialog1.FileName = "openFileDialog1";
+			this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.OpenFileDialog1FileOk);
+			// 
+			// flowLayoutPanel1
+			// 
+			this.flowLayoutPanel1.Location = new System.Drawing.Point(29, 59);
+			this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+			this.flowLayoutPanel1.Size = new System.Drawing.Size(558, 173);
+			this.flowLayoutPanel1.TabIndex = 1;
 			// 
 			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(810, 299);
+			this.Controls.Add(this.flowLayoutPanel1);
 			this.Controls.Add(this.button1);
 			this.Name = "MainForm";
 			this.Text = "GameBox v2.0";
--- GameBox v2.0/MainForm.cs
+++ GameBox v2.0/MainForm.cs
@@ -29,5 +29,45 @@ namespace GameBox_v2._
 			// TODO: Add constructor code after the InitializeComponent() call.
 			//
 		}
+		void Button1Click(object sender, EventArgs e)
+		{
+			openFileDialog1.ShowDialog();
+		}
+		void OpenFileDialog1FileOk(object sender, System.ComponentModel.CancelEventArgs e)
+		{
+			foreach(string a in openFileDialog1.FileNames)
+			{
+				additem(a);
+			}
+		}
+		void additem (string a){
+			
+			foreach ( Control c1 in flowLayoutPanel1.Controls ) {
+				if (c1.Name.Contains(a)){
+					goto pidano;
+				}
+			}
+				
+			if (a.Contains(".exe")){
+			
+				PictureBox pb = new PictureBox {Name = a,Size = new Size(300,150),BackgroundImageLayout = ImageLayout.Zoom, BackgroundImage = Icon.ExtractAssociatedIcon(a).ToBitmap()};
+				pb.Tag = flowLayoutPanel1.Controls.Count.ToString();
+				pb.MouseClick += Item_Click;
+				Label lb = new Label {Name = a + "1", Text = Path.GetFileName(a).Replace(".exe", string.Empty)};
+				lb.MouseClick += Item_Click;
+				lb.Font = new Font(lb.Font.Name, 24,FontStyle.Bold);
+				lb.AutoSize = true;
+				lb.BackColor = Color.Transparent;
+				pb.Controls.Add(lb);
+				
+				flowLayoutPanel1.Controls.Add(pb);
+					
+			}
+			goto mustek;
+			pidano: MessageBox.Show("Položka: <b>" + Path.GetFileName(a).Replace(".exe", string.Empty) + "<b> je již přidána!");
+			mustek:;
+			
+		}
+		
 	}
 }
