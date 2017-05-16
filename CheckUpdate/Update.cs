38f41fe54e5b80b73b6f0d6a518b3c73643e4680
GamerClassN7 <gamerclassn7@gmail.com>
2017-05-16 20:40:18 +0200

    Fix

--- CheckUpdate/Update.cs
+++ CheckUpdate/Update.cs
@@ -2,7 +2,7 @@
  * Created by SharpDevelop.
  * User: Asus
  * Date: 16. 5. 2017
- * Time: 20:28
+ * Time: 20:33
  * 
  * To change this template use Tools | Options | Coding | Edit Standard Headers.
  */
@@ -10,43 +10,34 @@ using System;
 
 namespace CheckUpdate
 {
-	class Program
+	/// <summary>
+	/// Description of Class1.
+	/// </summary>
+	public class Update
 	{
-		public static void Main(string[] args)
-		{
-			Console.WriteLine("Hello World!");
-			
-			// TODO: Implement Functionality Here
-			
-			Console.Write("Press any key to continue . . . ");
-			Console.ReadKey(true);
-		}
-	}
-	
-	class Update{
-		String name = "/Skype.exe";
+		String name = "/Game Launscher.exe";
 		String directory = System.Reflection.Assembly.GetExecutingAssembly().GetName().Name;
 		System.Net.WebClient wClient = new System.Net.WebClient();
 		String subject = directory + name;
 		System.Net.WebClient web = new System.Net.WebClient();
-		System.IO.Stream stream = web.OpenRead("");
+		System.IO.Stream stream = web.OpenRead("/test url/version.txt");
         System.IO.StreamReader reader = new System.IO.StreamReader(stream);
-        String version = //(jen čísla "2.0.0." ) Size proměné reader.ReadLine();
-        
-	public static void Start(){
+        String version = /*(jen čísla "2.0.0." ) Size proměné reader.ReadLine()*/;
+        	
+		public static void Start(){
         	if (version.Contains(this.ProductVersion)) {
 				foreach (System.Diagnostics.Process proces in System.Diagnostics.Process.GetProcesses()) {
 					if (proces.ProcessName == "Skipe"){
 						this.Close();
-						Get();
+						getUpdate();
 					} else {
-						Get();
+						getUpdate();
 					}
 				}
         	}
 		}
-		
-		private static void Get(){
+        
+        private void Get(){
 			if (System.IO.File.Exists(subject)){
 				System.IO.File.Delete(subject);
 			}
@@ -55,12 +46,11 @@ namespace CheckUpdate
             wClient.DownloadFileAsync(new Uri("link"), name);
         }
 
-        private static void Completed(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
+        private void Completed(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
         {
             MessageBox.Show("Download completed!");
             System.Diagnostics.Process.Start(directory + name);
             this.Close();
-        }
-		
+        } 
 	}
 }
